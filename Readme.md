a51b84c5ec08854bbcf47c5158a062ed273ab86d
rkycrwlr <rkycrwlr@proton.me>
2023-10-04 17:27:26 +0200

    Waterpixels Algorithm implemented

--- Readme.md
+++ Readme.md
@@ -6,8 +6,8 @@
 
 ### TODO :
 
-* Fast compute of dQ
-* Regularized Gradient
-* Apply watershed algorithm
+* ~~Fast compute of dQ~~
+* ~~Regularized Gradient~~
+* ~~Apply watershed algorithm~~
 * ~~Use hexagonal cells instead of square ones~~
 * Compute efficiency of the technique
--- waterpixel.py
+++ waterpixel.py
@@ -1,6 +1,7 @@
 #%%
 import numpy as np
 from scipy import ndimage
+import skimage
 import matplotlib.pyplot as plt
 from PIL import Image, ImageOps, ImageDraw
 import os
@@ -16,13 +17,14 @@ im = im.resize((400,300))
 np_img = np.array(im)
 CELL_RADIUS = 20
 RHO=2/3
+K=10
 #%%
 print(np_img.shape)
 
 #%%
 
 def morphological_grad(img, k_val=3, gauss_sigma=2):
-    if img.shape[-1] >= 2:
+    if img.ndim >= 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = ndimage.gaussian_filter(img,gauss_sigma)
     grad = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, np.ones((k_val, k_val)))
@@ -52,8 +54,12 @@ class Cell():
         self.values, self.mask = self.get_values()
         self.marker = self.get_marker()
         
+    
     def constrain(self,val,min_val, max_val):
-        return max(min_val, min(val,max_val))
+        return max(min_val, min(val,max_val))    
+
+    def get_center(self):
+        return (self.constrain(int(self.center[1]),0,self.img.shape[0]-1), self.constrain(int(self.center[0]),0,self.img.shape[1]-1))
 
     def get_values(self):
         msk = Image.new("L", (self.img.shape[1] + 2*self.rad, self.img.shape[0] + 2*self.rad))
@@ -109,68 +115,116 @@ def compute_cells(img, radius=CELL_RADIUS, rho=RHO):
             cells.append(Cell(center[1], center[0], radius, rho, img))
     return cells
 
+def get_glob_marker_center(cells):
+    glob_marker_center = np.zeros(cells[0].img.shape)
+    for i,cell in enumerate(cells):
+        glob_marker_center[cell.get_center()] = i+1
+    return glob_marker_center
+
+def get_glob_marker_distinct(cells):
+    glob_marker_distinct = np.zeros(cells[0].img.shape)
+    for i,cell in enumerate(cells):
+        glob_marker_distinct += cell.marker * (i+1)
+    return glob_marker_distinct
+
+
+
 cells = compute_cells(g_img)
-glob_mask = np.zeros(g_img.shape)
-glob_marker = np.zeros(g_img.shape)
-for cell in cells:
-    glob_mask += cell.mask
-    glob_marker += cell.marker
-plt.imshow(glob_mask, cmap='gray')
+glob_marker_center = get_glob_marker_center(cells)
+glob_marker_distinct = get_glob_marker_distinct(cells)
+
+plt.imshow(glob_marker_center, cmap='gray')
 plt.show()
-plt.imshow(glob_marker, cmap='gray')
+plt.imshow(glob_marker_distinct, cmap='gray')
 plt.show()
 
 #%%
 
-# N_cells = 8
-# rho = 2/3
-# img_paths = ["BSDS300/images/test/{}".format(elt) for elt in os.listdir("BSDS300/images/test")]
-# for i, img_path in enumerate(img_paths):
-#     np_img_grey_scale = np.array(ImageOps.grayscale(Image.open(img_path)))
-#     g_img = np.array(ndimage.morphological_gradient(np_img_grey_scale, size=(2,2)))
+def DF(a,b,i,j,dist_map):
+    return 2*abs(a*dist_map[i+a,j+b,1]) + 2*abs(b*dist_map[i+a,j+b,2]) + a**2 + b**2
 
-#     sigma = int(min(np_img_grey_scale.shape)/N_cells)
+def DL_distance_map(markers):
+    W,H = markers.shape
+    dist_map = np.zeros((W,H,3))
+    for i in range(W):
+        for j in range(H):
+            if markers[i,j] != 0:
+                dist_map[i,j] = np.array([0,0,0])
+            else:
+                dist_map[i,j] = np.array([np.inf,0,0])
 
-#     minima_g = compute_minima_g(g_img)
-#     global_mask, cells = compute_cells(minima_g)
-#     minima_g_cut = np.vectorize(lambda x: max(x,0))(minima_g - global_mask)
-    
-#     markers = np.zeros(np_img_grey_scale.shape, dtype=np.uint8)
-#     for k in range(len(cells)):
-#         for l in range(len(cells[0])):
-#             (totalLabels, label_ids, values, centroid) = cv2.connectedComponentsWithStats(cells[k,l])
-#             idx, maxarea = 0,0
-#             for j in range(1,totalLabels):
-#                 area = values[j, cv2.CC_STAT_AREA]
-#                 if area > maxarea:
-#                     idx = j
-#                     maxarea = area
-#             markers[int(sigma/2) + k*sigma - int(sigma/2*rho) : int(sigma/2) + k*sigma + int(sigma/2*rho), int(sigma/2) + l*sigma - int(sigma/2*rho) : int(sigma/2) + l*sigma +int(sigma/2*rho)] = (label_ids == idx) * (k * len(cells[0]+ l))
-    
-#     markers_coords = [[] for i in range(len(cells)*len(cells[0]))]
-#     for i in range(markers.shape[0]):
-#         for j in range(markers.shape[1]):
-#             if markers[i,j] > 0:
-#                 markers_coords[markers[i,j]].append((i,j))
+    V_minus = [(-1,-1),(-1,0),(-1,1),(0,-1)]
+    for i in range(1,W):
+        for j in range(1,H-1):
+            if markers[i,j] != 0:
+                continue
+            a,b = min((dist_map[i+elt[0],j+elt[1],0] + DF(elt[0],elt[1],i,j,dist_map), elt) for elt in V_minus)[1]
+            dist_map[i,j,1], dist_map[i,j,2] = dist_map[i+a,j+b,1]+a, dist_map[i+a,j+b,2]+b
+            dist_map[i,j,0] = dist_map[i+a,j+b,0] + DF(a,b,i,j,dist_map)
 
-#     ########## BEAUCOUP TROP LENT #############
-#     dQ = np.zeros(np_img_grey_scale.shape, dtype=np.float64)
-#     for i in range(np_img_grey_scale.shape[0]):
-#         for j in range(np_img_grey_scale.shape[1]):
-#             min_dist = np.inf
-#             for k in range(len(markers_coords)):
-#                 for elt in markers_coords[k]:
-#                     min_dist = min(((elt[0] - i)**2 + (elt[1] - j)**2)**0.5, min_dist)
-#             dQ[i,j] = min_dist
+    V_plus = [(1,1),(1,0),(1,-1),(0,1),(0,0)]
+    for i in range(W-2,-2,-1):
+        for j in range(H-2,-2,-1):
+            if markers[i,j] != 0:
+                continue
+            a,b = min((dist_map[i+elt[0],j+elt[1],0] + DF(elt[0],elt[1],i,j,dist_map), elt) for elt in V_plus)[1]
+            dist_map[i,j,1], dist_map[i,j,2] = dist_map[i+a,j+b,1]+a, dist_map[i+a,j+b,2]+b
+            dist_map[i,j,0] = dist_map[i+a,j+b,0] + DF(a,b,i,j,dist_map)
 
-#     dQ = 2/sigma * dQ
-#     plt.imsave("out_dQ.png", dQ, cmap="gray")
+    return dist_map[:,:,0]
+
+dist_map = DL_distance_map(glob_marker_center)
+print((2/CELL_RADIUS*np.sqrt(dist_map)).max())
+plt.imshow(np.sqrt(dist_map))
+plt.show()
+
+#%%
+
+def compute_reg_grad(g_img, dist_map, k=K, cell_dist=CELL_RADIUS):
+    return g_img + k * (2/cell_dist) * np.sqrt(dist_map)
+
+reg_g_img = compute_reg_grad(g_img*255, dist_map)
+
+plt.imshow(reg_g_img, cmap='gray')
+plt.show()
+
+#%%
+
+labels = skimage.segmentation.watershed(reg_g_img, glob_marker_distinct, watershed_line=True)
 
 
-#     # reg_grad
+def compute_segmentation(img, labels):
+    waterpix_mask = (labels == 0) * 255
+    if img.ndim >= 3:
+        waterpix_mask = np.tile(np.expand_dims(waterpix_mask, axis=-1),3)
+    waterpix_mask = skimage.morphology.dilation(waterpix_mask)
+    return np.where(waterpix_mask, waterpix_mask, img)
 
 
-#     # plt.imsave("out.png", markers, cmap="gray")
-#     # plt.imsave("out_b.png", minima_g_cut, cmap="gray")
+img_gray = cv2.cvtColor(np_img, cv2.COLOR_BGR2GRAY)
+plt.imshow(compute_segmentation(img_gray, labels), cmap='gray')
+plt.show()
 
-#     plt.imsave("gradients/{}.png".format(i), minima_g_cut, cmap="gray")
+#%%
+def waterpixel(img, cell_rad, k, rho, marker_center=False):
+    g_img = morphological_grad(img)
+    cells = compute_cells(g_img, radius=cell_rad, rho=rho)
+
+    if marker_center:
+        glob_marker = get_glob_marker_center(cells)
+        dist_map = DL_distance_map(glob_marker)
+    else:
+        glob_marker = get_glob_marker_distinct(cells)
+        dist_map = DL_distance_map(glob_marker)
+        
+    plt.imshow(reg_g_img)
+    plt.show()
+
+    labels = skimage.segmentation.watershed(reg_g_img, glob_marker, watershed_line=True)
+    waterpix_img = compute_segmentation(img,labels)
+    return waterpix_img
+
+# img_gray = cv2.cvtColor(np_img, cv2.COLOR_BGR2GRAY)
+w_img = waterpixel(np_img, 20, 25, 2/3)
+plt.imshow(w_img)
+plt.show()
